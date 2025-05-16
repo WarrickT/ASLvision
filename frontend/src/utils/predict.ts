c1f54c41d91e9c5c423948206480d5552e597428
Warrick Tsui <warricktsui1226@gmail.com>
2025-05-16 00:01:53 -0400

    Forgot to add frontend and backend connection before. Now added.

---
+++ frontend/src/utils/predict.ts
@@ -0,0 +1,37 @@
+import type { NormalizedLandmark } from "@mediapipe/hands";
+
+export async function sendLandmarksToBackend(
+    landmarks: NormalizedLandmark[]
+): Promise<string | null>{
+    if(landmarks.length !== 21){
+        console.warn("Should have 21 landmarks, but there's only ", landmarks.length);
+        return null;
+    }
+
+    const flattened = landmarks.flatMap((lm) => [lm.x, lm.y, lm.z]);
+    try{
+        const res = await fetch("http://localhost:8080/predict", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({landmarks: flattened}),
+        })
+
+        if(!res.ok){
+            console.error("Something happened: ", res.statusText);
+            return null;
+        }
+        const data = await res.json();
+
+        if(!data.letter){
+            console.warn("No letter in backend response, it's showing ", data);
+            return null;
+        }
+        return data.letter;
+    }
+    catch(err){
+        console.error("Fetch failed!", err);
+        return null;
+    }
+}
