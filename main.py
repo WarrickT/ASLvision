ee953207fbae10b88af6838cf73687dc17301eea
Warrick Tsui <warricktsui1226@gmail.com>
2025-05-16 19:03:32 -0400

    Added deployed frontend URL for CORS Middleware

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from typing import List
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173, https://aslvision-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
