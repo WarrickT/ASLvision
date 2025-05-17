324c55d1739b1782c8949d2e4030b51ffd2b2a27
Warrick Tsui <warricktsui1226@gmail.com>
2025-05-16 20:10:09 -0400

    Fixed CORS deployment bug

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from typing import List
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["http://localhost:5173, https://aslvision-frontend.onrender.com"],
+    allow_origins=["https://aslvision-frontend.onrender.com, http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
